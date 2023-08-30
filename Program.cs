b9e10ba691a352e11286b934e118d11d19887fb8
Aedan-Vongsouthi-Student <112557510+Aedan-Vongsouthi-Student@users.noreply.github.com>
2023-08-30 17:57:46 +0000

    Done

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ namespace NoviceChallenges
     {
         static void Main(string[] args)
         {
-            int[] testarray = {3,7,91,53,82,16,162};
-            Console.WriteLine(LargestInArray(testarray));
+            int[] testarray = {1,2,3,4,5,6,7,8};
+            char test = 'c';
+            Console.WriteLine(SwapEnds("Frankfurt"));
         }
 
         // 1. Return the sum of two numbers.
@@ -138,15 +139,33 @@ namespace NoviceChallenges
         // 11. Check if a string is a palindrome (reads the same forward and backward).
         public static bool IsPalindrome(string s)
         {
-            // TODO: Determine if the string is a palindrome.
-            return false;
+
+            char[] charArray = s.ToCharArray();
+            string reverse = "";
+            
+            for (int i = charArray.Length - 1; i >= 0; i--)
+            {
+                reverse += charArray[i];
+            }
+
+            if (reverse.ToLower() == s.ToLower())
+            {
+                return true;
+            } else {
+                return false;
+            }
         }
 
         // 12. Given an array of integers, return the sum of its elements.
         public static int ArraySum(int[] numbers)
         {
             int sum = 0;
-            // TODO: Calculate the sum of the array's elements.
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
             return sum;
         }
 
@@ -154,25 +173,51 @@ namespace NoviceChallenges
         public static int CharCount(string s, char c)
         {
             int count = 0;
-            // TODO: Count how many times character c appears in string s.
+            
+            string lower = s.ToLower();
+
+            for (int i = 0; i < lower.Length; i++)
+            {
+                if (lower[i] == c)
+                {
+                    count++;
+                }
+            }
+
             return count;
         }
 
         // 14. Given two strings, return a new string that is the concatenation of the two strings with a space in between.
         public static string ConcatenateStrings(string str1, string str2)
         {
-            // TODO: Concatenate the two strings with a space in between.
-            return "";
+            string twoString = str1 + " " + str2;
+            return twoString;
         }
 
         // 15. Given a string, return a new string where the first and last characters have been swapped.
         public static string SwapEnds(string s)
         {
             if (s.Length <= 1) return s;
+            
             char firstChar = s[0];
             char lastChar = s[s.Length - 1];
-            // TODO: Swap the first and last characters and return the modified string.
-            return "";
+
+            char[] charArray = s.ToCharArray();
+            char[] swappedArray = new char[s.Length];
+    
+            swappedArray[0] = lastChar;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                Console.WriteLine(swappedArray);
+                swappedArray[i] += charArray[i];
+            }
+
+            swappedArray[s.Length - 1] = firstChar;
+
+            string answer = new string(swappedArray);
+
+            return answer;
         }
     }
 }
